8f3426da3828771dae5d1a7372852be6c47f0068
Jack0Chan <48609550+Jack0Chan@users.noreply.github.com>
2021-06-11 00:14:22 +0800

    initialize p3

---
+++ 3EdgeSpliting/3EdgeSpliting/3EdgeSpliting.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<vector>
+
+using namespace std;
+
+class EdgeSplitter
+{
+public:
+    vector<vector<int>> originGraph;
+
+    void ReadInput();
+};
+
+void EdgeSplitter::ReadInput()
+{
+    int node1, node2;
+    while (cin >> node1 >> node2)
+    {
+        originGraph.push_back({ node1, node2 });
+    }
+}
+
+int main()
+{
+    EdgeSplitter edgeSplitter;
+    edgeSplitter.ReadInput();
+    return 0;
+}
